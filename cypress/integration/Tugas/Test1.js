1e8fb14d1000dce7ed85d9714626de5e9d8ec647
donasaputra <donasaputra@yahoo.com>
2021-08-25 20:41:33 +0700

    Update Study Case

--- cypress/integration/Tugas/Test1.js
+++ cypress/integration/Tugas/Test1.js
@@ -2,7 +2,14 @@
 
 describe ( 'My First Test Suite', function(){
     it('my first test case',function() {
-        cy.visit("https://www.npmtrends.com/");
+        cy.visit("https://bukalapak.com");
+        cy.contains('Daftar').click()
+        cy.get('.bl-text-field__inner')
+    })
+
+
+    Cypress.on('uncaught:exception',(err,runnable)=>{
+        return false
     })
 
 })
