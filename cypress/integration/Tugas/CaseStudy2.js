66ca9df27c5da9b9d8ce0bb6eb263a04e4dc9b17
donasaputra <donasaputra@yahoo.com>
2021-08-25 21:41:35 +0700

    Revisi code di  Study Case 2

--- cypress/integration/Tugas/CaseStudy2.js
+++ cypress/integration/Tugas/CaseStudy2.js
@@ -1,16 +1,34 @@
 
-describe ( 'My 2nd Test Suite', function(){
-    it('my second test case',function() {
-        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
-        cy.get('#et_pb_contact_name_0').type('Dona');
-        cy.get('#et_pb_contact_email_0').type('dona@saputra.com');
-        cy.get('.et_pb_contact_submit').click()
-        cy.get('[value="male"]').check();
-        cy.get('[value="Bike"]').click();
-        cy.get('select').select('Audi')
-        cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
-        cy.get('.entry-title').should('contain','Button success')
 
+describe ( 'Case Study 2', function(){
+    it('Submit Nama & Email',function() {
+        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
+        cy.get('#et_pb_contact_name_0').type('Dona').should('have.value','Dona');
+        cy.get('#et_pb_contact_email_0').type('dona@saputra.com').should('have.value','dona@saputra.com');
+        cy.get('.et_pb_contact_submit').contains('Email Me!');
+        cy.get('.et_pb_contact_submit').click();
+    })
+
+    it('Select Gender', function(){
+        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
+        cy.get('[value="male"]').check().should('be.checked');
+    })
+
+    it('Checkboxes',function(){
+        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
+        cy.get('[value="Bike"]').check().should('be.checked');
+        cy.get('[value="Car"]').check().should('be.checked');
+    })
+
+    it('Check Audi', function(){
+        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
+        cy.get('select').select('audi').should('have.value','audi')
+    })
+
+    it('Click Me Success', function(){
+        cy.visit("https://ultimateqa.com/simple-html-elements-for-automation/");
+        cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
+        cy.get('.entry-title').contains('Button success')
     })
 
 
